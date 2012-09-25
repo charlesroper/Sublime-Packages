57277664a9f3523c1aa571707bfd3c4e9f6e1748
Charles Roper <reachme@charlesroper.co.uk>
2012-09-25 23:02:51 +0100

    Initial working version of Journal.

--- User/open_journal.py
+++ User/open_journal.py
@@ -1,24 +1,31 @@
 import os
 import sublime
 import sublime_plugin
+from time import localtime, strftime
 
 
-class JournalCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
+class JournalCommand(sublime_plugin.ApplicationCommand):
+    def run(self):
         window = sublime.active_window()
         if os.path.exists("C:/Dropbox/journal.md"):
             view = window.open_file("C:/Dropbox/journal.md")
-            point = view.size()
+            if view.is_loading():
+                print "is loading"
+                sublime.set_timeout(lambda: self.run(), 100)
+            point = view.size() - 1
             while point >= 0:
                 if view.find("\S", point):
                     region = view.find("\s+", point)
                     edit = view.begin_edit()
-                    view.replace(edit, region, "\n\n")
-                    view.show(view.size())
+                    if region:
+                        view.replace(edit, region, "\n\n")
+                    else:
+                        view.insert(edit, point + 1, "\n\n")
+                    now = strftime("%H:%M", localtime())
+                    view.insert(edit, view.size(), "### %s\n\n" % now)
                     view.end_edit(edit)
-
+                    view.sel().clear()
+                    view.sel().add(sublime.Region(view.size()))
+                    view.show(point)
                     break
                 point -= 1
-
-
-
