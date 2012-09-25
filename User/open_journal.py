02666de8a5d9265b3d4418f8f2cbbe924c4bde58
Charles Roper <charles.roper@gmail.com>
2012-09-25 11:39:20 +0100

    Journal command.
    This is the initial iteration of this custom command. It removes trailing whitespace and adds two newlines to the end of the Journal.md file.

--- User/open_journal.py
+++ User/open_journal.py
@@ -1,5 +1,24 @@
-import sublime, sublime_plugin
+import os
+import sublime
+import sublime_plugin
 
-class ExampleCommand(sublime_plugin.TextCommand):
+
+class JournalCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
+        window = sublime.active_window()
+        if os.path.exists("C:/Dropbox/journal.md"):
+            view = window.open_file("C:/Dropbox/journal.md")
+            point = view.size()
+            while point >= 0:
+                if view.find("\S", point):
+                    region = view.find("\s+", point)
+                    edit = view.begin_edit()
+                    view.replace(edit, region, "\n\n")
+                    view.show(view.size())
+                    view.end_edit(edit)
+
+                    break
+                point -= 1
+
+
+
